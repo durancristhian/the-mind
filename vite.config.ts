2862d32ad3891e5d225b078e3119a3d943619d6f
Cristhian Duran <cristhian@logixboard.com>
2022-09-09 01:33:04 -0300

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
-})
+  plugins: [preact()],
+  /* https://github.com/evanw/esbuild/issues/2328#issuecomment-1159202149 */
+  esbuild: {
+    define: {
+      this: "window",
+    },
+  },
+});
